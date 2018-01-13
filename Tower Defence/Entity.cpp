507312fc7b557a14376d7ee749a18b87f8398497
Антон Казаков <anton_kazakov@outlook.com>
2018-01-13 13:16:15 +0300

    Добавлен класс "объект"

---
+++ Tower Defence/Entity.cpp
@@ -0,0 +1,39 @@
+#include "Entity.h"
+
+
+Entity::Entity(SDL_Texture * entityTexture, int X, int Y)
+{
+	texture = entityTexture;
+	rect.x = X;
+	rect.y = Y;
+	SDL_QueryTexture(texture, NULL, NULL, &rect.w, &rect.h);
+}
+
+
+Entity::Entity(SDL_Texture * entityTexture, int X, int Y, int Width, int Height)
+{
+	texture = entityTexture;
+	rect.x = X;
+	rect.y = Y;
+	rect.w = Width;
+	rect.h = Height;
+}
+
+
+Entity::~Entity()
+{
+
+}
+
+
+void Entity::Show(TD_Render * render)
+{
+	render->RenderTexture(texture, rect);
+}
+
+
+void Entity::Show(TD_Render * render, int src_X, int src_Y, int src_W, int src_H)
+{
+	SDL_Rect src = { src_X, src_Y, src_W, src_H};
+	render->RenderTexture(texture, src, rect);
+}
---
+++ Tower Defence/Entity.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <SDL.h>
+#include "Render.h"
+
+class Entity
+{
+	//Текстура объекта
+	SDL_Texture *texture;
+	//Координаты и размеры объекта
+	SDL_Rect rect;
+
+public:
+	/*
+	Конструктор объекта без масштабирования 
+	(размеры объекта равны размерам текстуры)
+	entityTexture - текстура объекта
+	X - Координата X объекта
+	Y - Координата Y объекта
+	*/
+	Entity(SDL_Texture *entityTexture, int X, int Y);
+
+
+	/*
+	Конструктор объекта с масштабированием
+	(размеры объекта вводятся вручную)
+	entityTexture - текстура объекта
+	X - Координата X объекта
+	Y - Координата Y объекта
+	*/
+	Entity(SDL_Texture *entityTexture, int X, int Y, int Width, int Height);
+
+
+	~Entity();
+	
+
+	/*
+	Рендер текстуры объекта
+	render - рендер в который будет выведена текстура
+	*/
+	virtual void Show(TD_Render *render);
+
+
+	/*
+	Рендер части текстуры объекта
+	render - рендер в который будет выведена текстура
+	src_X - Координата X части текстуры
+	src_Y - Координата Y части текстуры
+	src_W - Ширина части текстуры
+	src_H - Высота части текстуры
+	*/
+	virtual void Show(TD_Render *render, int src_X, int src_Y, int src_W, int src_H);
+};
+
+
