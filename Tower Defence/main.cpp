e4e7982bcc3a19e74d39108c5723663205438602
Антон Казаков <anton_kazakov@outlook.com>
2018-01-16 12:16:51 +0300

    Добавил комментарии

--- Tower Defence/main.cpp
+++ Tower Defence/main.cpp
@@ -4,15 +4,20 @@
 
 int main(int argc, char* args[])
 {
+	// Создание окна 
 	SDL_Window *window = SDL_CreateWindow("Tower Defence", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
-
+	
+	// Создание рендера
 	TD_Render render = TD_Render(window);
-
+	
+	// Инициализация приложения
 	App::Init(&render);
-
+	
+	//Создание сцены
 	App Scene1;
 
+	// Выполнение сцены
 	Scene1.Execution();
-
+	
 	return 0;
 }
